cd061dac35f9c2403c198b461b19025eb98203ef
wojtix144 <wojtix144@gmail.com>
2019-09-15 11:50:16 +0200

    meter style

--- src/meter.tsx
+++ src/meter.tsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import './meter.css'
+import cloud from '../assets/cloud.svg'
 
 function clamp(value) {
-    return Math.max(0, Math.min(100, value))
+    return Math.max(0, Math.min(99, value))
 }
 
 export function Meter(data) {
     const clamped = clamp(data.aqi)
     const arrowStyle = {
-        top: `calc(${clamped}% - ${clamped > 90 ? 20 : 0}px`
+        top: `calc(${clamped}% - ${clamped > 90 ? 52 : 0}px`
     }
     return <div className="meter">
-        <div className="alert meter-part"></div>
-        <div className="warning meter-part"></div>
-        <div className="good meter-part"></div>
-        <div className="arrow" style={arrowStyle}></div>
+        <div className="arrow" style={arrowStyle}>
+            <img src={cloud} />
+        </div>
     </div>
 }
